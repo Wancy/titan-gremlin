9576ddfd6320dcc51919910eb8e8252d106d592e
Wancy <flymydream2014@gmail.com>
2017-01-21 21:17:38 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bssh
-
+BIN=./bin
 # use cassandra backed db instead of berkeleyje
 # sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
 sed -i '' "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
@@ -12,4 +12,4 @@ index.search.elasticsearch.client-only=true
 storage.backend=cassandra
 storage.hostname=localhost" > conf/gremlin-server/titan-cassandra-server.properties
 
-$BIN/gremlin-server.sh conf/gremlin-server/gremlin-server.yaml
+$BIN/titan.sh start 
