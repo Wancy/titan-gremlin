5a56be0ddc1233305dad8a0283a2d86a32470dee
Wancy <flymydream2014@gmail.com>
2017-01-23 22:31:11 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bssh
 # use cassandra backed db instead of berkeleyje
-sed -i "s/host: localhost/host: 127.0.0.1/g" /opt/titan-1.0.0-hadoop1/conf/gremlin-server/gremlin-server.yaml
+sed -i "s/host: localhost/host: 0.0.0.0/g" /opt/titan-1.0.0-hadoop1/conf/gremlin-server/gremlin-server.yaml
 sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" /opt/titan-1.0.0-hadoop1/conf/gremlin-server/gremlin-server.yaml
 sed -i "s/channelizer: org.apache.tinkerpop.gremlin.server.channel.WebSocketChannelizer/channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer/g" /opt/titan-1.0.0-hadoop1/conf/gremlin-server/gremlin-server.yaml
 
@@ -13,5 +13,5 @@ index.search.elasticsearch.interface=NODE
 " > conf/gremlin-server/titan-cassandra-server.properties
 
 #sed -i "s/ELASTICSEARCH_IP:=127.0.0.1/ELASTICSEARCH_IP:=0.0.0.0/g" /opt/titan-1.0.0-hadoop1/bin/titan.sh
-#sed -i "s/GSRV_IP:=127.0.0.1/GSRV_IP:=0.0.0.0/g" /opt/titan-1.0.0-hadoop1/bin/titan.sh
+sed -i "s/GSRV_IP:=127.0.0.1/GSRV_IP:=0.0.0.0/g" /opt/titan-1.0.0-hadoop1/bin/titan.sh
  
